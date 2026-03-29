ffac0de183699df4088804ef2e120a102e08edbe
greenpanda-lol <180117139+greenpanda-lol@users.noreply.github.com>
2026-03-29 16:28:43 +0200

    Add main functionality for displaying pastas and implement safe mode filtering

---
+++ app.js
@@ -0,0 +1,34 @@
+const container = document.getElementById("container");
+
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+
+console.log("CATEGORY: " + category)
+
+if (category == "nsfw" && safeMode) {
+    window.location.href="/index.html";
+}
+
+fetch("/pastas.json")
+    .then(res => res.json())
+    .then(data => {
+        renderPastas(data);
+    });
+
+function renderPastas(data) {
+    let filtered = data;
+
+    if (category) {
+        filtered = data.filter(p => p.category === category);
+    }
+
+    if (safeMode) {
+        filtered = filtered.filter(p => !p.nsfw);
+    }
+
+    filtered.forEach(pasta => {
+        const el = document.createElement("pre");
+        el.textContent = pasta.content;
+        container.appendChild(el);
+    });
+}
